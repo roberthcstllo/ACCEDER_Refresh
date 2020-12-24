f40843e1915fcc25883ee2cf97fc0678782c6c3f
roberthcstllo <73672383+roberthcstllo@users.noreply.github.com>
2020-12-23 23:55:13 -0600

    First version commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgxUIModule } from '@swimlane/ngx-ui';
+import { MenuComponentComponent } from './menu-component/menu-component.component';
+
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoverComponentComponent } from './cover-component/cover-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponentComponent,
+    CoverComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxUIModule,
+    MatIconModule,
+    BrowserAnimationsModule
   ],
   providers: [],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
